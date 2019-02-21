3c706f9c7cb92928fe5e3e53eac59058ca704a14
Aditya <adityaaryan715@outlook.com>
2019-02-21 16:54:46 +0530

    commented extra things

--- game2.py
+++ game2.py
@@ -4,7 +4,7 @@ def guess_the_num():
     num = random.randint(1, 50)
     t=num-10
     u=num+10
-    print num
+    #print num
     count = 1
     while count != 4:
         guess = input("your should be between %d or %d:"%(t,u) )
