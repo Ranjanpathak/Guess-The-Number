50f672e651914b31556bcb198147665fd2b16ae9
Ranjanpathak <47792477+Ranjanpathak@users.noreply.github.com>
2019-02-21 16:50:55 +0530

    Add files via upload
    initial commit

---
+++ game2.py
@@ -0,0 +1,24 @@
+import random
+
+def guess_the_num():
+    num = random.randint(1, 50)
+    t=num-10
+    u=num+10
+    print num
+    count = 1
+    while count != 4:
+        guess = input("your should be between %d or %d:"%(t,u) )
+        if -5<(num-guess)<5:
+            print"guess is too close"
+        elif num < guess:
+            print"u r too high"
+        elif guess < num:
+            print"guess is too low"''
+        else:
+            print"you guessed it!"
+            break
+        count += 1
+    if count == 3:
+        print "u have reached maximum limit"
+
+guess_the_num()
