38a2dbaf8eeb402ed2cb603a9347f1e740b9f80b
Aditya <adityaaryan715@outlook.com>
2019-02-21 22:13:35 +0530

    Changes Made.

--- game2.py
+++ game2.py
@@ -2,23 +2,26 @@ import random
 
 def guess_the_num():
     num = random.randint(1, 50)
-    t=num-10
-    u=num+10
-    #print num
-    count = 1
+    div=num%10
+    t=num-div
+    u=t+10
+    print num
+    count = 0
     while count != 4:
         guess = input("your should be between %d or %d:"%(t,u) )
-        if -5<(num-guess)<5:
-            print"guess is too close"
-        elif num < guess:
-            print"u r too high"
-        elif guess < num:
-            print"guess is too low"''
+        if num-guess != 0:
+            if -2<(num-guess)<2:
+                print"guess is too close"
+        # elif num < guess:
+        #     print"u r too high"
+        # elif guess < num:
+        #     print"guess is too low"''
         else:
             print"you guessed it!"
             break
         count += 1
-    if count == 3:
-        print "u have reached maximum limit"
+        if count == 3:
+            print "u have reached maximum limit"
+            break
 
 guess_the_num()
